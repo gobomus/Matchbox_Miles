0763ce1390ae6b2ae7311c2a2871fd62584eebc8
noesclavo <spam@noesclavo.com>
2014-05-21 17:41:16 -0700

    more math

--- X_BaseShader/math.glsl
+++ X_BaseShader/math.glsl
@@ -19,12 +19,14 @@ void main()
 	c.x *= adsk_result_frameratio;
 
 	// max width of line
-	float max_width = 20.0;
 	// multiply max_width by distance from center to 0.0 (making line an even width as it 
 	// gets closer and further away
+	float max_width = 20.0;
+	max_width *= distance(c, vec2(0.0));
+
 	// use bc and gl_FragCoord.xy because there seems to be
 	// floating point erros in normalized space ?
-	vec2 v1 = vec2(bc - gl_FragCoord.xy)  / (max_width * distance(c, vec2(0.0)));
+	vec2 v1 = vec2(bc - gl_FragCoord.xy)  / max_width;
 
 	// if dot product == 0 then vectors are perpendicular
 	// if angle between vectors is less than 90 degrees, dot product it's positive
@@ -32,18 +34,27 @@ void main()
 	float dot1 = dot(v1,  c);
 	float dot2 = abs(dot1);
 
-	dot2 = smoothstep(.8, 1.0, dot2);
-
+	// Draw a line that is perpendicular to the center vector
 	vec3 col = vec3(0.0);
+	dot2 = smoothstep(.8, 1.0, dot2);
 	col += vec3(0.0, 1.0 - dot2, 0.0);
 
 	// this determines magnitude of vectors
 	float c_mag = sqrt(c.x * c.x + c.y + c.y);
 	float v_mag = sqrt(v1.x * v1.x + v1.y + v1.y);
 
-	if (c_mag * v_mag == dot1) {
-		col += vec3(0.0,0.0,1.0);
-	}
+	// I guess if the product of the 2 magnitudes is equal to the dot product
+	// of the 2 vectors, the vectors are parallel. Cant get it to work.
+	// if cross product is 0 they're also parallel, Can get that to work.
+	// don't know why it keeps the answer in the z channel though.
+	vec3 v1v = vec3(v1, 0.0);
+	vec3 c1v = vec3(c, 0.0);
+	vec3 cross1 = cross(c1v, v1v);
+
+	float parallel = 1.0 - abs(cross1.z);
+	parallel = smoothstep(0.0, .2, parallel);
+
+	col += vec3(0.0, 0.0, parallel);
 
 	c.x /= adsk_result_frameratio;
 
