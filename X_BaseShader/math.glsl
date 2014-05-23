91cb9458092817ac003e36450038a538a7bd79ef
noesclavo <spam@noesclavo.com>
2014-05-23 11:21:05 -0700

    tarred up a cleaned up math

--- X_BaseShader/math.glsl
+++ X_BaseShader/math.glsl
@@ -7,15 +7,12 @@ vec2 res = vec2(adsk_result_w, adsk_result_h);
 
 uniform vec2 center;
 uniform vec2 point2;
+uniform bool sick;
 
 
 const float pi = 3.141592653589793238462643383279502884197969;
 
 uniform float f_slider;
-
-//vec2 p1, p2, p3;
-//vec2 v1, v2,v3,v4;
-//vec3 v1v, v2v, v3v;
 
 float smooth = .1;
 
@@ -43,6 +40,14 @@ vec3 blue(float b)
 	return col;
 }
 
+vec3 white(float b)
+{
+	vec3 col = vec3(b);
+	col = smoothstep(0.0, smooth, col);
+
+	return col;
+}
+
 float mag(vec2 v) {
 	// find the magnitude of a vector
 	return sqrt(v.x * v.x + v.y * v.y);
@@ -58,7 +63,7 @@ float is_parallel(vec3 point_from_center, vec3 coords_from_center)
 	// by dividing the normalized vector by width we get scale the
 	// vector back. The close the vector is to the other one. The
 	// value of the cross product gets broader
-	float width = 20.0;
+	float width = f_slider;
 
 	// The z channel of the cross product holds the answer
 	// Not sure why
@@ -72,7 +77,7 @@ float is_perpendicular(vec2 point_from_center, vec2 coords_from_center)
 	// the dot product will return 0 if perpendicular and gets
 	// further away from 0 the less perpendicular
 	float scale_vector = 1000.0;
-	float width = 20.0;
+	float width = f_slider;
 
 	float dot1 = 1.0 - abs(dot(normalize(point_from_center) / width, coords_from_center * scale_vector));
 
@@ -81,14 +86,10 @@ float is_perpendicular(vec2 point_from_center, vec2 coords_from_center)
 
 float draw_cross(vec3 center_vector, vec3 coords_from_center)
 {
-
 	float col = is_parallel(center_vector, coords_from_center);
-
 	col = clamp(col, 0.0, 1.0);
 
-
 	return col;
-
 }
 
 bool draw_a_line(vec2 v1, vec2 v2, vec2 v3, vec2 v4)
@@ -105,6 +106,13 @@ float get_angle(vec2 center_to_point2, vec2 coords_from_center)
 	float angle = acos(dot(center_to_point2, coords_from_center) / (mag(center_to_point2) * mag(coords_from_center)));
 
 	return angle;
+}
+
+float draw_circle(vec2 v2, vec2 v3)
+{
+	float circle =  1.0 - smoothstep(length(v2) - smooth*smooth, length(v2), length(v3));
+
+	return circle;
 }
 
 void main()
@@ -136,19 +144,6 @@ void main()
 	float perp = is_perpendicular(v2, v3);
 	col += green(perp);
 
-
-	// draw center cross
-	vec3 center_offset = vec3(v1v - vec3(0.0, v1v.y, 0.0));
-	float center_cross = draw_cross(v1v - center_offset, v3v);
-
-	center_offset = vec3(v1v - vec3(v1v.x, 0.0, 0.0));
-	center_cross += draw_cross(v1v - center_offset, v3v);
-	col += red(center_cross);
-
-	if (draw_a_line(v1, v2, v3, v4)) {
-		col += red(para);
-	}
-
 	// Find the angle between 2 vectors
 	float angle_in_radians = get_angle(v2, v3);
 	float angle_in_degrees = degrees(angle_in_radians);
@@ -159,8 +154,31 @@ void main()
 		angle_in_degrees = 360.0 - angle_in_degrees;
 	}
 
+	if (sick) {
+		col += blue(sin(angle_in_degrees));
+	}
+
 	// View the angle analyze with a cc node to see the values go from 0 - 360
 	//col = vec3(0.0, 0.0, angle_in_degrees);
+
+	float circle = draw_circle(v2, v3);
+
+	col += clamp(green(circle) * (angle_in_degrees / 360.0), 0.0, 1.0);
+
+	col = clamp(col, 0.0, 1.0);
+
+	// draw center cross
+	vec3 center_offset = vec3(v1v - vec3(0.0, v1v.y, 0.0));
+	float center_cross = draw_cross(v1v - center_offset, v3v);
+
+	center_offset = vec3(v1v - vec3(v1v.x, 0.0, 0.0));
+	center_cross += draw_cross(v1v - center_offset, v3v);
+	col += red(center_cross);
+
+	if (draw_a_line(v1, v2, v3, v4)) {
+		//col += red(para);
+		col += white(para);
+	}
 
 	gl_FragColor = vec4(col, 1.0);
 }
