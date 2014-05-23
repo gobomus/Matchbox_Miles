d37f410822b0cb54472e3686644a783db68d8137
noesclavo <spam@noesclavo.com>
2014-05-23 08:27:19 -0700

    finally figuring out math mo beta

--- X_BaseShader/math.glsl
+++ X_BaseShader/math.glsl
@@ -13,25 +13,101 @@ const float pi = 3.141592653589793238462643383279502884197969;
 
 uniform float f_slider;
 
+vec2 p1, p2, p3;
+vec2 v1, v2,v3,v4;
+vec3 v1v, v2v, v3v;
 
-vec2 p2;
-vec2 v2,v3,v4;
+float smooth = .1;
 
+vec3 red(float r)
+{
+	vec3 col =  vec3(r, 0.0, 0.0);
+	col = smoothstep(0.0, smooth, col);
 
+	return col;
+}
+
+vec3 green(float g)
+{
+	vec3 col = vec3(0.0, g, 0.0);
+	col = smoothstep(0.0, smooth, col);
+
+	return col;
+}
+
+vec3 blue(float b)
+{
+	vec3 col = vec3(0.0, 0.0, b);
+	col = smoothstep(0.0, smooth, col);
+
+	return col;
+}
 
 float mag(vec2 v) {
 	return sqrt(v.x * v.x + v.y * v.y);
 }
 
-vec2 uniform_scale(vec2 st, vec2 center, float scale)
+float is_parallel(vec3 point_from_center, vec3 coords_from_center)
 {
-    vec2 ss = vec2(scale / vec2(100.0));
+	// scale the vector so the cross product can be visualized sharper
+	float scale_vector = 1000.0;
 
-    return (st - center) / ss + center;
+	// by dividing the normalized vector by width we get scale the
+	// vector back. The close the vector is to the other one. The
+	// value of the cross product gets broader
+	float width = 20.0;
+
+	// The z channel of the cross product holds the answer
+	// Not sure why
+	float para = 1.0 - abs(cross(normalize(point_from_center) / width, coords_from_center * scale_vector).z);
+
+	return para;
+}
+
+float is_perpendicular(vec2 point_from_center, vec2 coords_from_center)
+{
+	float scale_vector = 1000.0;
+	float width = 20.0;
+
+	float dot1 = 1.0 - abs(dot(normalize(point_from_center) / width, coords_from_center * scale_vector));
+
+	return dot1;
+}
+
+void main()
+{
+	vec2 st = gl_FragCoord.xy / res;
+
+	vec3 col = vec3(0.0);
+
+	p1 = center;
+	p2 = point2;
+	p3 = st;
+
+	v1 = p1;
+	v2 = p1 - p2;
+	v2.x *= adsk_result_frameratio;
+	v3 = p1 - st;
+	v3.x *= adsk_result_frameratio;
+
+	v1v = vec3(v1, 0.0);
+	v2v = vec3(v2, 0.0);
+	v3v = vec3(v3, 0.0);
+
+	float para = is_parallel(v2v, v3v);
+	col += blue(para);
+
+	float perp = is_perpendicular(v2, v3);
+	col += green(perp);
+
+
+
+
+	gl_FragColor = vec4(col, 1.0);
 }
 
 
-void main()
+int old_main()
 {
 	vec2 st = gl_FragCoord.xy / res;
 	//vec2 point2 = vec2(0.0);
@@ -149,12 +225,27 @@ void main()
 
 	mw = length(p2 - c);
 
-	// HERE
+	//works
 	cross1 = cross(c1v, v1v);
 
 	if (length(v1) < length(v3) && length(v2) < length(v3) && abs(cross1.z / mag(v3)) < .01) {
 			col += vec3(1.0);
 	}
+	//end works
+
+
+	dot1 = dot(v1*100000.00, v3);
+
+	dot2 = 1.0 - abs(dot1);
+	dot2 = smoothstep(0.0, .00001, dot2);
+
+	//col = vec3(0.0);
+	if (abs(dot1) < 100) {
+		//col = vec3(0.0,0.0,dot1);
+	}
+
+
+
 
 	// make center cross
 	if (abs(center.x - st.x) < max_width/adsk_result_w) {
@@ -164,8 +255,11 @@ void main()
 	}
 
 
+
 	c.x /= adsk_result_frameratio;
 
 
 	gl_FragColor = vec4(col, 1.0);
+
+	return 1;
 }
