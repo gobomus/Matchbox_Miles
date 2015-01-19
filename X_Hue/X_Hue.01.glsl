7181f615238b6d1ad0043f6d0ffa2a447be9d14d
Miles Essmiller <miles@noesclavo.com>
2015-01-19 08:57:13 -0800

    Updated X_Hue

--- X_Hue/X_Hue.01.glsl
+++ X_Hue/X_Hue.01.glsl
@@ -1,207 +1,115 @@
 #version 120
 
-#define INPUT Front
-#define tex(col, coords) texture2D(col, coords).rgb
+uniform sampler2D Front, Matte;
+uniform float adsk_result_w, adsk_result_h;
 
-
-uniform sampler2D INPUT;
-uniform float adsk_result_w, adsk_result_h, ratio;
 vec2 res = vec2(adsk_result_w, adsk_result_h);
 
-const float pi = 3.141592653589793238462643383279502884197969;
+uniform float r, g, b, c, m, y;
+uniform float rs, gs, bs, cs, ms, ys;
+uniform float f;
 
-uniform float in_g;
+// hsv conversion from here: http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
 
-uniform float shiftr, rfo, rs;
-uniform float shiftg, gfo, gs;
-uniform float shiftb, bfo, bs;
-uniform float shiftc, cfo, cs;
-uniform float shiftm, mfo, ms;
-uniform float shifty, yfo, ys;
-uniform float saturation;
-
-vec3 toyiq(vec3 col)
+vec3 rgb2hsv(vec3 c)
 {
-	mat3 ym = mat3(
-        .299, .587, .114,
-        .596, -.274, -.321,
-        .211, -.523, .311
-    );
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = c.g < c.b ? vec4(c.bg, K.wz) : vec4(c.gb, K.xy);
+    vec4 q = c.r < p.x ? vec4(p.xyw, c.r) : vec4(c.r, p.yzx);
 
-	col *= ym;
-
-	return col;
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
 }
 
-vec3 fromyiq(vec3 col)
+vec3 hsv2rgb(vec3 c)
 {
-	mat3 rm = mat3(
-		1.0, .956, .621,
-		1.0, -.272, -.647,
-		1.0, -1.107, 1.705
-	);
-
-	col *= rm;
-
-	return col;
-}
-
-vec3 rhue(vec3 col, float r)
-{	
-	float rmult = 180.0;
-	//rmult = 1.0;
-
-	mat3 hm = mat3(
-		1.0,	0.0,	0.0,
-		0.0,	cos(r * pi / rmult),	-sin(r * pi / rmult),
-		0.0,	sin(r * pi / rmult),	cos(r * pi / rmult)
-	);
-
-	col *= hm;
-
-	return col;
-}
-
-vec3 adjust_saturation(vec3 col, float s)
-{
-	//saturation
-	mat3 sm = mat3(
-		1.0, 0.0, 0.0,
-		0.0, s, 0.0,
-		0.0, 0.0, s
-	);
-
-	col *= sm;
-
-	return col;
-}
-
-
-vec3 hue_ops(vec3 col, float r, float s, float v)
-{
-	//rgb to yiq
-	mat3 ym = mat3(
-		.299, .587, .114,
-		.596, -.274, -.321,
-		.211, -.523, .311
-	);
-
-	float rmult = 180.0;
-	rmult = 1.0;
-
-	//hue rotation
-	mat3 hm = mat3(
-		1.0,	0.0,	0.0,
-		0.0,	cos(r * pi / rmult),	-sin(r * pi / rmult),
-		0.0,	sin(r * pi / rmult),	cos(r * pi / rmult)
-	);
-
-	//saturation
-	mat3 sm = mat3(
-		1.0, 0.0, 0.0,
-		0.0, s, 0.0,
-		0.0, 0.0, s
-	);
-
-	//value
-	mat3 vm = mat3(
-		v, 0.0, 0.0,
-		0.0, v, 0.0,
-		0.0, 0.0, v
-	);
-
-	//yiq to rgb
-	mat3 rm = mat3(
-		1.0, .956, .621,
-		1.0, -.272, -.647,
-		1.0, -1.107, 1.705
-	);
-
-	col = col * ym * hm * sm * vm * rm;
-
-	return col;
-}
-
-float get_hue(vec3 col)
-{
-	col = clamp(col, 0.0, 1.0);
-
-	//compute hue from rgb ... Damn cool
-	float h = atan(sqrt(3.0) * (col.g - col.b), 2.0 * col.r - col.g - col.b);
-	h = degrees(h);
-	if (h < 0.0) {
-		h += 360.0;
-	}
-
-	return h;
-}
-
-vec3 go(vec3 col, float g, int op)
-{
-	if (op == 0) {
-		col = pow(col, vec3(g));
-	} else {
-		col = pow(col, vec3(1.0 / g));
-	}
-
-	return col;
-}
-
-vec3 shift_hue(vec3 col, float the_hue, float shift, float fo, float sat)
-{
-	float in_g = 1.0;
-
-	float th = 360 - the_hue;
-	float dif = -(th - 180.0);
-
-	col = toyiq(col);
-	col = rhue(col, dif);
-	th += dif;
-	col = fromyiq(col);
-
-	float h = get_hue(col);
-	col = toyiq(col);
-
-	float s = 0.0;
-
-	if (h > th) {
-		s = (1.0 - smoothstep(th, th + fo, h)) * shift;
-	} else {
-		s = smoothstep(th - fo, th, h) * shift;
-	}
-
-	col = rhue(col, s);
-	//col = adjust_saturation(col, s * sat + 1.0);
-	col = rhue(col, -dif);
-	col = fromyiq(col);
-
-
-	return col;
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 }
 
 void main(void)
 {
+	/*
+	yellow .1663
+	cyan .5
+	green .33325
+	magenta .83301
+	red 0.0
+	blue .66650
+	*/
+
+	float rv = 0.0;
+	float yv = .16663;
+	float gv = .33325;
+	float cv = .5;
+	float bv = .66650;
+	float mv = .83301;
+
 	vec2 st = gl_FragCoord.xy / res;
-	vec3 col = tex(INPUT, st);
-	col = go(col, in_g, 0);
 
-	vec4 hues[] = vec4[](
-		vec4(0.0, -shiftr, rfo, rs),
-		vec4(60.0, -shifty, yfo, ys),
-		vec4(120.0, -shiftg, gfo, gs),
-		vec4(180.0, -shiftc, cfo, cs),
-		vec4(240.0, -shiftb, bfo, bs),
-		vec4(300.0, -shiftm, mfo, ms)
-	);
+	vec3 col = texture2D(Front, st).rgb;
+	float mt = texture2D(Matte, st).r;
 
-	for (int i = 0; i < 6; i++) {
-		col = shift_hue(col, hues[i].r, hues[i].g, hues[i].b, hues[i].a);
+	vec3 hsv = rgb2hsv(col);
+
+	float h = hsv.r;
+	float s = hsv.g;
+	float v = hsv.b;
+
+	float ydiff = 1.0 - abs(h - yv);
+	float yh = mix(1.0, h, y + 1.0);
+
+	float gdiff = 1.0 - abs(h - gv);
+	float gh = mix(1.0, h, g + 1.0);
+
+	float cdiff = 1.0 - abs(h - cv);
+	float ch = mix(1.0, h, c + 1.0);
+
+	float bdiff = 1.0 - abs(h - bv);
+	float bh = mix(1.0, h, b + 1.0);
+
+	float mdiff = 1.0 - abs(h - mv);
+	float mh = mix(1.0, h, m + 1.0);
+
+	float rdiff = 1.0 - abs(h - rv);
+	float rh = mix(1.0, h, r + 1.0);
+
+	if (ydiff > f) {
+		h = mix(h, yh, ydiff);
+		s = mix(s, s * ys, ydiff);
 	}
 
+	if (gdiff > f) {
+		h = mix(h, gh, gdiff);
+		s = mix(s, s * gs, gdiff);
+	}
+	
+	if (cdiff > f) {
+		h = mix(h, ch, cdiff);
+		s = mix(s, s * cs, cdiff);
+	}
+	
+	if (bdiff > f) {
+		h = mix(h, bh, bdiff);
+		s = mix(s, s * bs, bdiff);
+	}
 
-	col = go(col, in_g, 1);
+	if (mdiff > f) {
+		h = mix(h, mh, mdiff);
+		s = mix(s, s * ms, mdiff);
+	}
+	
+	if (rdiff > f) {
+		h = mix(h, rh, rdiff);
+		s = mix(s, s * rs, rdiff);
+	}
 
-	gl_FragColor = vec4(col, 1.0);
+	hsv.r = h;
+	hsv.g = s;
+
+	col = mix(col, hsv2rgb(hsv), mt);
+
+	gl_FragColor.rgb = col;
 }
-
-
